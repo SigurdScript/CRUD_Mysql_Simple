16e51618017de5e35578927963b2e1796549963b
Camilo Vargas <bomilo66@gmail.com>
2021-01-20 12:17:21 -0500

    [ADD] ANSWER NOT FOUND

--- src/index.js
+++ src/index.js
@@ -5,6 +5,10 @@ app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 
 app.use('/api', require('./routes'))
+app.use('*', (req, res, next) => {
+    res.status(404).json({ messahe: 'PAGE NOT FOUND' })
+    next()
+})
 ;(() => {
     app.listen(3000, () => {
         console.log('Server on port: 3000')
